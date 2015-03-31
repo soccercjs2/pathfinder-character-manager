57038af58050fa11c362f2322d9305b44ae1fe43
soccercjs2 <soccercjs2@gmail.com>
2015-03-30 17:41:40 -0700

    Added files to source control

---
+++ Pathfinder/Controllers/AbilityController.cs
@@ -0,0 +1,78 @@
+﻿using Pathfinder.Models;
+using Pathfinder.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pathfinder.Controllers
+{
+    public class AbilityController : Controller
+    {
+        private PathfinderContext db = new PathfinderContext();
+
+        // GET: Ability
+        public ActionResult Index(int id)
+        {
+            return View(new AbilityViewer(id));
+        }
+
+        public ActionResult CreateAbility(int id)
+        {
+            Ability ability = new Ability();
+            ability.CharacterId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAbility(Ability ability)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Abilities.Add(ability);
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Abilities", new { Id = ability.CharacterId });
+            }
+            else
+            {
+                return View(ability);
+            }
+        }
+
+        public ActionResult EditAbility(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditAbility(Ability ability)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Abilities.Attach(ability);
+                db.Entry(ability).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Abilities", new { Id = ability.CharacterId });
+            }
+            else
+            {
+                return View(ability);
+            }
+        }
+
+        public ActionResult DeleteAbility(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeleteAbility(Ability ability)
+        {
+            return View();
+        }
+    }
+}
---
+++ Pathfinder/Models/Ability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pathfinder.Models
+{
+    public class Ability
+    {
+        public int AbilityId { get; set; }
+        public int CharacterId { get; set; }
+        public int AbilityTypeId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsConditional { get; set; }
+    }
+}
