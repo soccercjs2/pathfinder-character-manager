8626c36a2785d41d90ca158d10e75005b531f137
soccercjs2 <soccercjs2@gmail.com>
2015-04-28 07:33:30 -0700

    random

--- Pathfinder/Scripts/pathfinder.js
+++ Pathfinder/Scripts/pathfinder.js
@@ -1,5 +1,4 @@
-﻿
-function GoToRoller(name, value) {
+﻿function GoToRoller(name, value) {
     var url = '/Character/Roller/' + name + '/\'1d20\'p' + value;
     window.open(url, '_blank', 'width=400,height=140,resizable=1');
 }
@@ -31,5 +30,28 @@ function ToggleRowsVisible(name) {
         // options
         columnWidth: 285,
         itemSelector: '.masonry-box'
+    });
+};
+
+function UpdateAbility(abilityId, characterId, abilityTypeId, name, description, isConditional, active) {
+    var ability = {
+        AbilityId: abilityId,
+        CharacterId: characterId,
+        AbilityTypeId: abilityTypeId,
+        Name: name,
+        Description: description,
+        IsConditional: isConditional,
+        Active: active
+    }
+
+    $.ajax({
+        type: 'POST',
+        dataType: 'text',
+        url: "/Character/UpdateAbility",
+        data: ability,
+        dataType: 'json',
+        success: function (response) {
+            window.location.href = response.Url;
+        }
     });
 };
