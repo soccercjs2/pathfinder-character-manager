34f70acf9976d7b9d9983a6d23d218eb171a6b38
soccercjs2 <soccercjs2@gmail.com>
2015-07-08 13:40:21 -0700

    Comment equation controller

--- Pathfinder/Controllers/EquationController.cs
+++ Pathfinder/Controllers/EquationController.cs
@@ -13,27 +13,30 @@ namespace Pathfinder.Controllers
     {
         private PathfinderContext db = new PathfinderContext();
         
-        // GET: Equation
         public ActionResult Index(int id)
         {
+            //to see all equations for a character
             return View(new EquationViewer(id));
         }
 
         [HttpPost]
         public ActionResult Index(EquationViewer viewer)
         {
+            //selected a category to filter equation results
             return View(new EquationViewer(viewer.CharacterId, viewer.CategoryId));
         }
 
         public ActionResult Create(int id)
         {
+            //load categories for create equation dropdown
             List<EquationCategory> categories = db.EquationCategories.Where(m => m.CharacterId == id).ToList<EquationCategory>();
             ViewBag.Categories = new SelectList(categories, "EquationCategoryId", "Name");
             
+            //initialize equation variables
             Equation equation = new Equation();
             equation.CharacterId = id;
-            equation.AbilityId = 0;
-            equation.ShowFormula = true;
+            equation.AbilityId = 0; //AbilityId is 0 because the equation is not tied to an ability
+            equation.ShowFormula = true; //this is a base equation, so show it
 
             return View(equation);
         }
@@ -43,6 +46,7 @@ namespace Pathfinder.Controllers
         {
             if (ModelState.IsValid)
             {
+                //add equation to db so that it can be sorted with all of the other shown equations
                 equation.EvaluationOrder = db.Equations.Max(e => e.EvaluationOrder);
                 db.Equations.Add(equation);
                 db.SaveChanges();
@@ -50,31 +54,37 @@ namespace Pathfinder.Controllers
                 //if equations are invalid and can't be sorted, remove the equation, and go back to editing it
                 if (!SortEquations(equation.CharacterId))
                 {
+                    //remove the equation
                     db.Equations.Remove(db.Equations.Find(equation.EquationId));
                     db.SaveChanges();
 
+                    //go back to editing
                     return View(equation);
                 }
                 else
                 {
+                    //valid equation saved, so go back to equation index
                     return RedirectToAction("Index", "Equation", new { Id = equation.CharacterId });
                 }
             }
             else
             {
+                //equation was missing something, go back to editing
                 return View(equation);
             }
         }
 
         public ActionResult CreateAbilityEquation(int id)
         {
+            //find the abilty to add an equation to
             Ability ability = db.Abilities.Find(id);
             
+            //initialize the equation
             Equation equation = new Equation();
             equation.CharacterId = ability.CharacterId;
-            equation.EquationCategoryId = 0;
+            equation.EquationCategoryId = 0; //equation doesn't have a category because it's tied to an ability
             equation.AbilityId = id;
-            equation.ShowFormula = false;
+            equation.ShowFormula = false; //equation is not a base equation because it's tied to an ability, and shouldn't be shown
 
             return View(equation);
         }
@@ -84,6 +94,7 @@ namespace Pathfinder.Controllers
         {
             if (ModelState.IsValid)
             {
+                //add the equation and set the name
                 equation.Name = equation.BonusType;
                 equation.EvaluationOrder = db.Equations.Max(e => e.EvaluationOrder);
                 db.Equations.Add(equation);
@@ -92,24 +103,29 @@ namespace Pathfinder.Controllers
                 //if equations are invalid and can't be sorted, remove the equation, and go back to editing it
                 if (!SortEquations(equation.CharacterId))
                 {
+                    //remove the equation
                     db.Equations.Remove(db.Equations.Find(equation.EquationId));
                     db.SaveChanges();
 
+                    //go back to editing
                     return View(equation);
                 }
                 else
                 {
+                    //valid equation added, go back to ability page
                     return RedirectToAction("AbilityBonuses", "Ability", new { Id = equation.AbilityId });
                 }
             }
             else
             {
+                //something is missing, go back to editing
                 return View(equation);
             }
         }
 
         public ActionResult EditAbilityEquation(int id)
         {
+            //find ability equation, and go to edit page
             return View(db.Equations.Find(id));
         }
 
@@ -121,6 +137,7 @@ namespace Pathfinder.Controllers
                 //keep equation to revert back to if new equation isn't valid
                 Equation oldSavedEquation = db.Equations.Find(equation.EquationId);
 
+                //add the equation and set the equation name
                 equation.Name = equation.BonusType;
                 db.Equations.Attach(equation);
                 db.Entry(equation).State = EntityState.Modified;
@@ -129,27 +146,34 @@ namespace Pathfinder.Controllers
                 //if equations are invalid and can't be sorted, reattach the old equation, and go back to editing the new equation
                 if (!SortEquations(equation.CharacterId))
                 {
+                    //attach the old equation to overwrite changes
                     db.Equations.Attach(oldSavedEquation);
                     db.Entry(oldSavedEquation).State = EntityState.Modified;
                     db.SaveChanges();
 
+                    //return to editing
                     return View(equation);
                 } 
                 else
                 {
+                    //valid equation added, return to ability page
                     return RedirectToAction("AbilityBonuses", "Ability", new { Id = equation.AbilityId });
                 }                
             }
             else
             {
+                //something is missing, go back to editing
                 return View(equation);
             }
         }
 
         public ActionResult CreateCategory(int id)
         {
+            //initialize category
             EquationCategory category = new EquationCategory();
             category.CharacterId = id;
+
+            //go to creationg page
             return View(category);
         }
 
@@ -158,18 +182,23 @@ namespace Pathfinder.Controllers
         {
             if (ModelState.IsValid)
             {
+                //add equation category
                 db.EquationCategories.Add(category);
                 db.SaveChanges();
+
+                //continue to equation index
                 return RedirectToAction("Index", new { Id = category.CharacterId });
             }
             else
             {
+                //something is missing, go back to editing
                 return View(category);
             }
         }
 
         public ActionResult Edit(int id)
         {
+            //find equation and go to edit page
             return View(db.Equations.Find(id));
         }
 
@@ -181,6 +210,7 @@ namespace Pathfinder.Controllers
                 //keep equation to revert back to if new equation isn't valid
                 Equation oldSavedEquation = db.Equations.Find(equation.EquationId);
 
+                //update the equation
                 db.Equations.Attach(equation);
                 db.Entry(equation).State = EntityState.Modified;
                 db.SaveChanges();
@@ -188,34 +218,46 @@ namespace Pathfinder.Controllers
                 //if equations are invalid and can't be sorted, reattach the old equation, and go back to editing the new equation
                 if (!SortEquations(equation.CharacterId))
                 {
+                    //overwrite equation changes with the original equation
                     db.Equations.Attach(oldSavedEquation);
                     db.Entry(oldSavedEquation).State = EntityState.Modified;
                     db.SaveChanges();
 
+                    //go back to editing
                     return View(equation);
                 }
                 else
                 {
-                    return RedirectToAction("AbilityBonuses", "Ability", new { Id = equation.AbilityId });
+                    //valid equation added, go back to equation index
+                    return RedirectToAction("Index", "Equation", new { Id = equation.CharacterId });
                 }    
             }
             else
             {
+                //something is missing, go back to editing
                 return View(equation);
             }
         }
 
         private bool SortEquations(int characterId)
         {
+            //load shown equations and their names seperately
             List<Equation> equations = db.Equations.Where(m => m.CharacterId == characterId && m.ShowFormula == true).ToList<Equation>();
             List<string> equationNames = equations.Select(e => e.Name).ToList<string>();
+
+            //prepare to track when you find a valid equation
             List<string> validatedEquationNames = new List<string>();
             int validationAttempts = 0;
+
+            //this is the limit of tries the sorter has to sort the equations
             int validationAttemptsMax = equations.Count * 4;
             
+            //loop through equations to validate each of them
             for (int i = 0; i < equations.Count; i++ )
             {
+                //continue as long as the sorter hasn't reached it's validation limit
                 if (validationAttempts < validationAttemptsMax) {
+                    //track when you have found an invalid equation in the formula
                     bool hasInvalidEquations = false;
                     validationAttempts++;
 
@@ -270,6 +312,7 @@ namespace Pathfinder.Controllers
                 }
             }
 
+            //you've found nothing wrong, continue!
             return true;
         }
     }
