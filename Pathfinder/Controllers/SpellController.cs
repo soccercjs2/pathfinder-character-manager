52e806c9ec8b0d9c18e4e533499714d2c42064c6
soccercjs2 <soccercjs2@gmail.com>
2015-08-22 12:29:15 -0700

    Fixing new files

---
+++ Pathfinder/Controllers/SpellController.cs
@@ -0,0 +1,185 @@
+﻿using Pathfinder.Models;
+using Pathfinder.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pathfinder.Controllers
+{
+    public class SpellController : Controller
+    {
+        private PathfinderContext db = new PathfinderContext();
+
+        public ActionResult Index(int id)
+        {
+            List<Spellbook> spellbooks = db.Spellbooks.Where(m => m.CharacterId == id).ToList<Spellbook>();
+            return View(spellbooks);
+        }
+
+        public ActionResult CreateSpellbook(int id)
+        {
+            Spellbook spellbook = new Spellbook();
+            spellbook.CharacterId = id;
+            return View(spellbook);
+        }
+
+        [HttpPost]
+        public ActionResult CreateSpellbook(Spellbook spellbook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Spellbooks.Add(spellbook);
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Spell", new { Id = spellbook.CharacterId });
+            }
+            else
+            {
+                return View(spellbook);
+            }
+        }
+
+        public ActionResult EditSpellbook(int id)
+        {
+            return View(db.Spellbooks.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult EditSpellbook(Spellbook spellbook)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Spellbooks.Attach(spellbook);
+                db.Entry(spellbook).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Spell", new { Id = spellbook.CharacterId });
+            }
+            else
+            {
+                return View(spellbook);
+            }
+        }
+
+        public ActionResult KnownSpells(int id)
+        {
+            return View(new KnownSpellsView(id));
+        }
+
+        public ActionResult CastSpells(int id)
+        {
+            return View(new CastSpellsViewasdf(id));
+        }
+
+        public ActionResult CreateSpellLevel(int id)
+        {
+            SpellLevel knownSpellsByLevel = new SpellLevel();
+            knownSpellsByLevel.SpellbookId = id;
+            return View(knownSpellsByLevel);
+        }
+
+        [HttpPost]
+        public ActionResult CreateSpellLevel(SpellLevel knownSpellsByLevel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SpellLevels.Add(knownSpellsByLevel);
+                db.SaveChanges();
+
+                return RedirectToAction("KnownSpells", "Spell", new { Id = knownSpellsByLevel.SpellbookId });
+            }
+            else
+            {
+                return View(knownSpellsByLevel);
+            }
+        }
+
+        public ActionResult EditSpellLevel(int id)
+        {
+            return View(db.SpellLevels.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult EditSpellLevel(SpellLevel knownSpellsByLevel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SpellLevels.Attach(knownSpellsByLevel);
+                db.Entry(knownSpellsByLevel).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("KnownSpells", "Spell", new { Id = knownSpellsByLevel.SpellbookId });
+            }
+            else
+            {
+                return View(knownSpellsByLevel);
+            }
+        }
+
+        public ActionResult CreateSpell(int id)
+        {
+            Spell spell = new Spell();
+            spell.SpellbookId = id;
+            return View(spell);
+        }
+
+        [HttpPost]
+        public ActionResult CreateSpell(Spell spell)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Spells.Add(spell);
+                db.SaveChanges();
+
+                return RedirectToAction("KnownSpells", "Spell", new { Id = spell.SpellbookId });
+            }
+            else
+            {
+                return View(spell);
+            }
+        }
+
+        public ActionResult EditSpell(int id)
+        {
+            return View(db.Spells.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult EditSpell(Spell spell)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Spells.Attach(spell);
+                db.Entry(spell).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("KnownSpells", "Spell", new { Id = spell.SpellbookId });
+            }
+            else
+            {
+                return View(spell);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult UpdateSpell(Spell spell)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Spells.Attach(spell);
+                db.Entry(spell).State = System.Data.Entity.EntityState.Modified;
+
+                db.SaveChanges(); //asdf
+
+                return Json(new { });
+            }
+            else
+            {
+                return Json(new { });
+            }
+        }
+    }
+}
---
+++ Pathfinder/ViewModels/CastSpellsView.cs
@@ -0,0 +1,47 @@
+﻿using Pathfinder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pathfinder.ViewModels
+{
+    public class CastSpellsView
+    {
+        public Spellbook Spellbook { get; set; }
+        public List<SpellLevel> SpellLevels { get; set; }
+        public Dictionary<int, List<Spell>> PreparedSpells { get; set; }
+
+        private PathfinderContext db = new PathfinderContext();
+
+        public CastSpellsView() { }
+        public CastSpellsView(int id)
+        {
+            this.Spellbook = db.Spellbooks.Find(id);
+
+            this.SpellLevels = db.SpellLevels
+                .Where(m => m.SpellbookId == this.Spellbook.SpellbookId)
+                .OrderBy(m => m.Level)
+                .ToList<SpellLevel>();
+
+            this.PreparedSpells = LoadPreparedSpells();
+        }
+
+        private Dictionary<int, List<Spell>> LoadPreparedSpells()
+        {
+            Dictionary<int, List<Spell>> preparedSpells = new Dictionary<int, List<Spell>>();
+
+            foreach (var item in this.SpellLevels)
+            {
+                List<Spell> preparedSpellsAtLevel = db.Spells
+                    .Where(m => m.SpellbookId == this.Spellbook.SpellbookId && m.Level == item.Level && m.Prepared > 0)
+                    .OrderBy(m => m.Name)
+                    .ToList<Spell>();
+
+                preparedSpells.Add(item.Level, preparedSpellsAtLevel);
+            } //asdf
+
+            return preparedSpells;
+        }
+    }
+}
